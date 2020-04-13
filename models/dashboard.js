3bc1cdc1d7d2239b1c4d9d565a15c360c677dfc4
“PotatoDreamsgit config --global user.email “cvthai@student.unimeb.edu.au <35085921+PotatoDreams@users.noreply.github.com>
2020-04-13 12:22:28 +1000

    Adding data models

---
+++ models/dashboard.js
@@ -0,0 +1,17 @@
+const dashboard = [
+    {
+        user: "food",
+        energySaved: 12, //in KJ
+        carbonSaved: 2.1, //in KG
+        streak: 10 // days
+    },
+
+    {
+        user: "yoyo",
+        energySaved: 0, //in KJ
+        carbonSaved: 0, //in KG
+        streak: 0 // days
+    }
+]
+
+module.exports = dashboard; 
---
+++ models/route.js
@@ -0,0 +1,21 @@
+module.export[
+  {
+    user: "food",
+    origin: "Home",
+    destination: "School",
+    distance: 2,
+    duration: 60,
+    completed: 1
+
+  },
+
+  {
+    user: "food",
+    origin: "School",
+    destination: "Home",
+    distance: 2,
+    duration: 60,
+    completed: 0
+
+  }
+]
