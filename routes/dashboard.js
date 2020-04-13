32f675d24e049ef728c9bed4341e6fd962243cec
Nathan Harvey <neathtthefirst@gmail.com>
2020-04-13 09:29:27 +0800

    add routes

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,2 +1,11 @@
-const express = require('express');
+//   /  folder
 
+const express = require("express");
+
+const dashboardRouter = express.Router();
+
+const authorController = require("../controllers/dashboard.js");
+
+dashboardRouter.get('/', (req, res) => dashboardRouter.home(req, res));
+
+module.exports = authorRouter;
---
+++ routes/forum.js
@@ -0,0 +1,9 @@
+//   /forum  folder
+
+const express = require("express");
+
+const forumRouter = epxress.Router();
+
+const forumController = require("../controllers/forum.js");
+
+forumRouter.get('')
