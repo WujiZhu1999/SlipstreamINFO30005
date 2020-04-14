8e612438bc7940256c80286107e219007ca22a97
Nathan Harvey <neathtthefirst@gmail.com>
2020-04-14 21:25:15 +0800

    added user routes

--- controllers/users.js
+++ controllers/users.js
@@ -1,25 +1,85 @@
 var users = require("../models/users.js");
 
 const createUser = (req, res) => {
-    if (req.body.name == null || req.body.username == null || req.body.password == null){
+    if (req.body.name == null || req.body.userName == null || req.body.password == null){
+        res.status(400)
         res.send("incomplete data");
-        res.send(req.body.name);
-        res.send(req.body.username);
-        res.send(req.body.password);
         return;
     }
 
-    if (users.find((user) => user.username === req.body.username) != null) {
+    if (users.find((user) => user.userName === req.body.userName) != null) {
+        res.status(400)
         res.send("user already exists");
         return;
     }
 
     users.push({
         "name":req.body.name,
-        "username":req.body.username,
+        "userName":req.body.userName,
         "passsword":req.body.password
     });
-    res.send(users);
+    res.send()
 }
 
-module.exports = {createUser,};
+const deleteUser = (req, res) => {
+    if (users.find((user) => user.userName === req.params.userName) == null){
+        res.status(400)
+        res.send("user does not exist " + req.params.userName);
+        return;
+    }
+
+    users.splice(users.findIndex((user) => user.userName === req.params.userName), 1);
+    res.send()
+}
+
+const getUser = (req, res) => {
+    if (users.find((user) => user.userName === req.params.userName) == null){
+        res.status(400)
+        res.send("user does not exist " + req.body.userName);
+        return;
+    }
+
+    res.send(users.find((user) => user.userName === req.params.userName))
+}
+
+const changeUser = (req, res) => {
+    if (users.find((user) => user.userName === req.params.userName) == null){
+        res.status(400);
+        res.send("user does not exist " + req.body.userName);
+        return;
+    }
+
+    if (req.body.name == null && req.body.password == null && req.body.userName == null){
+        res.status(400);
+        res.send("cannot make no change");
+    }
+
+
+    var user = users.find((user) => user.userName === req.params.userName);
+
+    if (req.body.name != null){
+        user["name"] = req.body.name;
+    }
+
+    if (req.body.password != null){
+        user["password"] = req.body.password;
+    }
+
+    if (req.body.userName != null){
+        if (users.find((user) => user.userName === req.body.userName) != null){
+            res.status(400);
+            res.send("can't change username, user already exists");
+            return;
+        }
+        user["userName"] = req.body.userName;
+    }
+
+    res.send(user);
+}
+
+module.exports = {
+    createUser,
+    deleteUser,
+    getUser,
+    changeUser
+};
--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,8 @@ const usersRouter = express.Router();
 const usersController = require("../controllers/users.js");
 
 usersRouter.post('/', (req, res) => usersController.createUser(req, res));
+usersRouter.delete('/:userName', (req, res) => usersController.deleteUser(req, res));
+usersRouter.get('/:userName', (req, res) => usersController.getUser(req, res));
+usersRouter.post('/:userName', (req, res) => usersController.changeUser(req, res));
 
 module.exports = usersRouter;
