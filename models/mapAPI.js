0628af991e0776a41a7ba2dc44e94c33eeda0fe1
Nathan Harvey <neathtthefirst@gmail.com>
2020-04-15 13:56:25 +0800

    add fake api database

---
+++ models/mapAPI.js
@@ -0,0 +1,10 @@
+const fakeAPI = [
+    {"to":"perth",
+    "toID":2,
+    "fromID":3,
+    "from":"melbourne",
+    "route":"go here then go there then go to destination"
+    },
+];
+
+module.exports = fakeAPI;
