9c8a7f9f1073d5c8bf191ea5743d0a87a1be813d
pweaus <pweaus@gmail.com>
2020-04-19 21:26:27 +0800

    dashboard add border

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,5 +1,5 @@
-const dashboard = require("../models/dashboard");
-function getdata(displaynum){
+const dashboard = require("../models/dashboard"); 
+function getdata(displaynum){    
     var x=1;
     var content="";
     if(displaynum > dashboard.length){ displaynum=dashboard.length} //display number
@@ -11,13 +11,14 @@ function getdata(displaynum){
         content += "<dd>"+ dashboard[i].streak + " days Streak </dd>";
         
     }
-    var page = '<div><h1>Leaderboard</h1><dl id="Leaderboard"></dl></div><script>document.getElementById("Leaderboard").innerHTML="'+ content +'";</script>'
-    return page;
+    var page = '<style> .divcss5{width:300px;height:300px;border:2px solid #000} dt{border-top:2px solid #000}</style><div class="divcss5"><h1 align="center">Leaderboard</h1><dl id="Leaderboard"></dl></div>\
+    <script>document.getElementById("Leaderboard").innerHTML="'+ content +'";</script>'
+    return page;  
 }
 
 const home = (req, res) => {
     if (true){//logged in 
-        res.send(getdata(dashboard.length));
+        res.send(getdata(dashboard.length));  
     } else {
         res.send("<h1> logged out homepage\nWelcome to Slipstream</h1>");
     }
