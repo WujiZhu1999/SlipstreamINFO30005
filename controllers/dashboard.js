5c364c4ca5671b0bf5b3a1110e891b557364ec96
pweaus <pweaus@gmail.com>
2020-04-25 15:16:20 +0800

    friends leaderboard sorted by distance

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -3,27 +3,11 @@
 const dashboard = require("../models/dashboard"); 
 const users = require("../models/users.js");
 const friendsController = require("../controllers/friends.js");
-
-function getdata(displaynum){    
-    var x=1;
-    var content="";
-    if(displaynum > dashboard.length){ displaynum=dashboard.length} //display number
-    for (var i=0;i<displaynum;i++){
-        var index =i+1;
-        content += "<dt>"+ index + "</dt> <dd>"+ dashboard[i].user +"</dd>";
-        content += "<dd>"+ dashboard[i].energySaved + " KJ energy Saved </dd>";
-        content += "<dd>"+ dashboard[i].carbonSaved + " KG carbon Saved </dd>";
-        content += "<dd>"+ dashboard[i].streak + " days Streak </dd>";
-        
-    }
-    var page = '<style> .divcss5{width:300px;height:300px;border:2px solid #000} dt{border-top:2px solid #000}</style><div class="divcss5"><h1 align="center">Leaderboard</h1><dl id="Leaderboard"></dl></div>\
-    <script>document.getElementById("Leaderboard").innerHTML="'+ content +'";</script>';
-    return page;  
-}
+const displaynum = 2 //display number
 
 const home = (req, res) => {
     if (req.session.user != null){//logged in 
-        res.send(getdata(dashboard.length));  
+        res.send("");  
     } else {
         res.send("<h1> logged out homepage<br>Welcome to Slipstream</h1>");
     }
@@ -32,10 +16,11 @@ const home = (req, res) => {
 //Ranks a user's friends and themselves on the totalDistance cycled
 const getLeaderboard = (req,res) => {
     //title for the leaderbaord
-    var output = "<h1>Leaderboard</h1>";
+    var output = "<h1 align='center'>Leaderboard</h1>";
 
     //gets a list of usernames that should be ranked
-    const userName = req.session.user;
+    // const userName = req.session.user;
+    const userName = "hello"
     if( users.find((user) => user.userName === userName) == false){
         res.send("not valid user")
         return;
@@ -62,23 +47,28 @@ const getLeaderboard = (req,res) => {
 
    var ranking = 0
 
+   if(sortedDistance.length < displaynum){displaynum=sortedDistance.length}
    // Creating the output for the leaderboard based on sortedDistance rankings 
     for(distance in sortedDistance){
         var userIndex = 0;
 
-        for(userNum in userList){
+        if (userIndex>=displaynum){
+            break;
+        }
 
+        for(userNum in userList){
+                
             const user = users.find((user) => user.userName === userList[userNum]);
 
-            if (distance = user.data.totalDistance){
+            if (sortedDistance[distance] == user.data.totalDistance){
 
                     ranking += 1;
-                    const addUser = "<br>" + ranking + ". " + user.name + " DISTANCE: " + user.data.totalDistance;
-                    output += addUser;
-
+                    // const addUser = "<br>" + ranking + ". " + user.name + " DISTANCE: " + user.data.totalDistance;
+                    // output += addUser;
+                    output += "<dt>"+ ranking + "</dt> <dd>"+ user.name +"</dd><dd> DISTANCE: "+ user.data.totalDistance +"</dd><dd>"+user.data.energySaved+" KJ energy Saved </dd>"
+                    + "<dd>"+ user.data.carbonSaved + " KG carbon Saved </dd>" + "<dd>"+ user.data.streak + " days Streak </dd>"
                     userList.splice((userIndex), 1); 
-                    break;
-                    
+                    break;      
             }
 
             else{
@@ -88,6 +78,8 @@ const getLeaderboard = (req,res) => {
                 
         }
     }
+    output = '<style> .divcss5{width:300px;height:600px;border:2px solid #000} dt{border-top:2px solid #000}</style><div class="divcss5"><dl id="Leaderboard"></dl></div>\
+    <script>document.getElementById("Leaderboard").innerHTML="'+ output +'";</script>'
     res.send(output);
 }
 
