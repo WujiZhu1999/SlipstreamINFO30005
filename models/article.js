38c7073bc15f41dd8fa10b29fc6d9e082b8fd3a6
“PotatoDreamsgit config --global user.email “cvthai@student.unimeb.edu.au <35085921+PotatoDreams@users.noreply.github.com>
2020-04-30 22:22:15 +1000

    Create, Edit, Delete Comments

--- models/article.js
+++ models/article.js
@@ -5,14 +5,34 @@ const articles = [
         "articleNum" : 1,
         "title" : "I RODE MY BIKE",
         "body" : "MY BIKE IS SO GOOD I RODE IT",
-        "author" : "food"
+        "author" : "food",
+        "comments": [
+            {
+                "commentNumber": 1,
+                "commentBody": "great job!",
+                "commentAuthor": "yoyo"
+            },
+
+            {
+                "commentNumber": 2,
+                "commentBody": "thank you!",
+                "commentAuthor": "food"
+            },
+
+        ]
     },
 
     {
         "articleNum" : 2,
         "title" : "It was raining today...",
         "body" : "The rain today meant I could not ride my bike. Hopefully tomorrow it is sunny!",
-        "author" : "food"
+        "author" : "food",
+        "comments": {
+            "commentNumber": 1,
+            "commentBody": "days like that are the worst!",
+            "commentAuthor": "yoyo"
+        }
+        
     }
 ]
 
